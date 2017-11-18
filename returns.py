7a5546f74035a13e9bec81f00d0d19c912cba17f
henryblah <dragonsboy@hotmail.co.uk>
2017-11-18 11:45:39 +0000

    Basic stats and graphing of historical S&P500 returns.

--- returns.py
+++ returns.py
@@ -6,7 +6,10 @@ start_date = "2010-01-01"
 end_date = "2017-11-17"
 
 adjclose = data.DataReader("^gspc", "yahoo", start_date, end_date)["Adj Close"]
-plt.figure(figsize=(20,10))
 returns = adjclose.pct_change(1)
+print(returns.describe())
+print("Skewness {}".format(returns.skew()))
+print("Excess Kurtosis {}".format(returns.kurt()))
+plt.figure(figsize=(15,10))
 returns.plot(grid = True)
 plt.show()
